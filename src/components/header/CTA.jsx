7929df5d6f599490ece320fe2bc868d8cc08d8a7
pragnavi <pragnavi1398@gmail.com>
2024-08-12 19:20:11 -0400

    Fixed Download CV Button

--- src/components/header/CTA.jsx
+++ src/components/header/CTA.jsx
@@ -1,10 +1,10 @@
 import React from 'react'
-import CV from '../../assets/cv.pdf'
+import CV from '../../assets/Pragnavi_RavuluriSaiDurga.pdf'
 
 const CTA = () => {
   return (
     <div className='cta'>
-        <a href={CV} download className='btn'>Download CV</a>
+        <a href={CV} download='Pragnavi_RavuluriSaiDurga.pdf' className='btn'>Download CV</a>
         <a href="#contact" className='btn btn-primary'>Contact Me</a>
     </div>
   )
